0d39c6d9960376acdee1b42c35ee9e2a5a4d771b
Joe Peaden <joepeaden@Joes-MBP.attlocal.net>
2023-03-08 08:09:54 -0500

    Started to add shop uI

--- Assets/Scripts/MenuUI.cs
+++ Assets/Scripts/MenuUI.cs
@@ -13,6 +13,6 @@ public class MenuUI : MonoBehaviour
         // ugh. sometimes I just don't have the brainpower to do it all and some code ends up kinda yuck.
         // I'd really rather not just straight up grab it, I'd rather a more elegant way. But I'm workin hard man!
         // and since it's just me - fuck it. That's right. fuck it.
-        scoreText.text = "You made $" + Scoreboard.totalScore + ".00";
+        scoreText.text = "Your family will receive $" + Scoreboard.totalScore + ".00";
     }
 }
